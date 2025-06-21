775101b2f7739d7a016e94d568ecf32a95908cb9
拾音 <150756244+Heronesukun@users.noreply.github.com>
2025-06-21 17:09:13 +0800

    Update config.py
    选择模型和添加apikey

--- config.py
+++ config.py
@@ -1,17 +1,9 @@
-'''
-Description: 配置文件
-Author: 嘎嘣脆的贝爷
-Date: 2025-02-14 21:56:01
-LastEditTime: 2025-02-17 22:57:26
-LastEditors: 嘎嘣脆的贝爷
-'''
-
 
 class Config:
     # 本地的简历名称
     resume_name = 'resume.md'
     # Google AI Studio API配置
-    api_key = 'AIzaSyC8Rt30A_nZsEcV17r-ZG_MnpCxdQPDVx4'  # 替换为您的API密钥
+    api_key =   # 替换为您的API密钥
     # 模型配置
     think_model = 'gemini-2.0-flash'  # 思考模型
     chat_model = 'gemini-2.0-flash'   # 聊天模型
